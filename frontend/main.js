812874c7838586389da2966463e51081bf00df23
sudoxer <azzamkulovshokhruz@gmail.com>
2026-03-19 08:39:21 +0500

    web-prog-my-city

--- frontend/main.js
+++
@@ -1,9 +0,0 @@
-// Небольшой JS без бизнес-логики: плавная прокрутка к секции категорий
-const categoryAnchor = document.querySelector('a[href="#categories"]');
-
-if (categoryAnchor) {
-  categoryAnchor.addEventListener("click", (event) => {
-    event.preventDefault();
-    document.querySelector("#categories")?.scrollIntoView({ behavior: "smooth" });
-  });
-}
